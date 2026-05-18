303c44d1903a15128832ad5c1504d1c4b01847a8
HitmanII1234 <soyhitmanvizz@gmail.com>
2026-05-17 20:05:31 -0600

    ya funciona menu, falta corregir options y multiplayer

--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
@@ -9,11 +10,153 @@ public class MenuController : MonoBehaviour
     public GameObject panelNombres;
     public TMP_InputField inputName;
 
+    [Header("UI Opciones")]
+    public GameObject panelOpciones;
+    public Slider sliderMaster;
+    public Slider sliderMusic;
+    public Slider sliderSFX;
+    public AudioSource musicSource;
+    public AudioSource[] sfxSources;
+    public Toggle fullscreenToggle;
+    public Dropdown qualityDropdown;
+    public GameObject[] optionSubPanels;
+
     [Header("UI Game Over")]
     public GameObject panelGameOver;
     public TextMeshProUGUI textoScoreFinal;
 
     private bool esMultijugador = false;
+    private float masterVolume = 1f;
+    private float musicVolume = 1f;
+    private float sfxVolume = 1f;
+
+    private void Start()
+    {
+        LoadAudioSettings();
+    }
+
+    public void AbrirOpciones()
+    {
+        if (panelOpciones != null)
+            panelOpciones.SetActive(true);
+    }
+
+    public void CerrarOpciones()
+    {
+        if (panelOpciones != null)
+            panelOpciones.SetActive(false);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        masterVolume = Mathf.Clamp01(volume);
+        AudioListener.volume = masterVolume;
+        PlayerPrefs.SetFloat("MasterVolume", masterVolume);
+        UpdateMusicVolume();
+        UpdateSFXVolume();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicVolume = Mathf.Clamp01(volume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        UpdateMusicVolume();
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxVolume = Mathf.Clamp01(volume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+        UpdateSFXVolume();
+    }
+
+    public void SetFullscreen(bool fullScreen)
+    {
+        Screen.fullScreen = fullScreen;
+        PlayerPrefs.SetInt("Fullscreen", fullScreen ? 1 : 0);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("QualityLevel", qualityIndex);
+    }
+
+    public void ResetOptionsToDefault()
+    {
+        SetMasterVolume(1f);
+        SetMusicVolume(1f);
+        SetSFXVolume(1f);
+
+        if (fullscreenToggle != null)
+            fullscreenToggle.isOn = true;
+
+        if (qualityDropdown != null)
+            SetQuality(Mathf.Max(qualityDropdown.options.Count - 1, 0));
+    }
+
+    public void OpenOptionSubPanel(int index)
+    {
+        if (optionSubPanels == null)
+            return;
+
+        for (int i = 0; i < optionSubPanels.Length; i++)
+        {
+            if (optionSubPanels[i] != null)
+                optionSubPanels[i].SetActive(i == index);
+        }
+    }
+
+    private void LoadAudioSettings()
+    {
+        masterVolume = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        musicVolume = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        sfxVolume = PlayerPrefs.GetFloat("SFXVolume", 1f);
+
+        AudioListener.volume = masterVolume;
+        UpdateMusicVolume();
+        UpdateSFXVolume();
+
+        if (sliderMaster != null)
+            sliderMaster.value = masterVolume;
+
+        if (sliderMusic != null)
+            sliderMusic.value = musicVolume;
+
+        if (sliderSFX != null)
+            sliderSFX.value = sfxVolume;
+
+        if (fullscreenToggle != null)
+            fullscreenToggle.isOn = PlayerPrefs.GetInt("Fullscreen", Screen.fullScreen ? 1 : 0) == 1;
+
+        if (qualityDropdown != null)
+        {
+            int maxQualityIndex = Mathf.Max(qualityDropdown.options.Count - 1, 0);
+            int savedQuality = PlayerPrefs.GetInt("QualityLevel", QualitySettings.GetQualityLevel());
+            savedQuality = Mathf.Clamp(savedQuality, 0, maxQualityIndex);
+            qualityDropdown.value = savedQuality;
+            qualityDropdown.RefreshShownValue();
+            QualitySettings.SetQualityLevel(savedQuality);
+        }
+    }
+
+    private void UpdateMusicVolume()
+    {
+        if (musicSource != null)
+            musicSource.volume = masterVolume * musicVolume;
+    }
+
+    private void UpdateSFXVolume()
+    {
+        if (sfxSources != null)
+        {
+            foreach (AudioSource source in sfxSources)
+            {
+                if (source != null)
+                    source.volume = masterVolume * sfxVolume;
+            }
+        }
+    }
 
     public void BotonModoHistoria()
     {
